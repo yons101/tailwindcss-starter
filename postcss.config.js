0243445aab10d2cfb7e6d534b74479fb35d32bb0
yons101 <45919766+yons101@users.noreply.github.com>
2020-10-24 22:38:09 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,28 +3,31 @@ const plugins = {
     autoprefixer: {}
 };
 
-plugins["@fullhuman/postcss-purgecss"] = {
-    content: [
-        "public/index.html"
-        // "pages/**/*.tsx",
-        // "components/**/*.tsx",
-        // "pages/**/*.js",
-        // "components/**/*.js"
-    ],
+if (process.env.NODE_ENV === "production") {
+    plugins["@fullhuman/postcss-purgecss"] = {
+        content: [
+            "public/index.html"
+            // "pages/**/*.tsx",
+            // "components/**/*.tsx",
+            // "pages/**/*.js",
+            // "components/**/*.js"
+            //etc
+        ],
 
-    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
-};
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    };
 
-plugins["cssnano"] = {
-    preset: [
-        "default",
-        {
-            discardComments: {
-                removeAll: true
+    plugins["cssnano"] = {
+        preset: [
+            "default",
+            {
+                discardComments: {
+                    removeAll: true
+                }
             }
-        }
-    ]
-};
+        ]
+    };
+}
 module.exports = {
     plugins
 };
